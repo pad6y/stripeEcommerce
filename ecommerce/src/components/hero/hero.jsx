f86cd7a36ad4e8ec2f0896d55bc134112e57d11a
Patrick Wong <saihowong81@hotmail.com>
2022-05-14 17:25:49 +0100

    Hero link fixed

--- ecommerce/src/components/hero/hero.jsx
+++ ecommerce/src/components/hero/hero.jsx
@@ -1,13 +1,20 @@
+import { useHistory } from 'react-router-dom';
 import './hero.styles.scss';
 
 function Hero() {
+  const history = useHistory();
+
   return (
     <section className="hero is-large is-info hero-image">
       <div className="hero-body">
         <div className="container">
           <h1 className="hero-title">Made for living</h1>
           <div className="shop-now-btn">
-            <button className="button is-black" id="shop-now">
+            <button
+              className="button is-black"
+              id="shop-now"
+              onClick={() => history.push('/shop')}
+            >
               SHOP NOW
             </button>
           </div>
